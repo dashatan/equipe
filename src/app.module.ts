bb2ff19d97259c01dff2a7d36fcbb7d9f29eb1ee
abbas <dashatanad@gmail.com>
2023-08-16 15:57:51 +0330

    progress

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: '127.0.0.1',
+      port: 27017,
+      username: '',
+      password: '',
+      database: 'test', 
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,14 @@
-export class User {}
+import { Column, ObjectId, Entity, ObjectIdColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @ObjectIdColumn() id: ObjectId;
+  @Column() username: string;
+  @Column() password: string;
+  @Column() email: string;
+  @Column() name: string;
+
+  constructor(user?: Partial<User>) {
+    Object.assign(this, user);
+  }
+}
